9c033b35c46ba6111c9950b44aa17685ac96304a
akash <17wh1a0593@bvrithyderabad.edu.in>
2020-03-02 11:32:23 +0530

    added some code to the class

--- MavenOops/src/main/java/Com/Epam/MavenOops/NewYear.java
+++ MavenOops/src/main/java/Com/Epam/MavenOops/NewYear.java
@@ -1,5 +1,15 @@
 package Com.Epam.MavenOops;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 public class NewYear {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Map<String,Float> mp = new HashMap<String,Float>();
+		mp.put("FiveStar",(float) 0.000); 
+		System.out.println(mp.get("FiveStar"));
+					/*put("MilkyBar", 0);
+					put("Rasgulla", 0);
+					put("Laddo", 0);*/
+	}
 }
